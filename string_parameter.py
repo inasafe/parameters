c03cb398bae30c75b1caeb34c8175d4407147fc3
Ismail Sunni <imajimatika@gmail.com>
2014-08-26 21:42:41 +0700

    Update string parameter.

--- string_parameter.py
+++ string_parameter.py
@@ -5,14 +5,18 @@ from generic_parameter import GenericParameter
 
 
 class StringParameter(GenericParameter):
-        """A subclass of generic parameter that accepts string only.
+    """A subclass of generic parameter that accepts string only.
 
-        .. versionadded:: 2.2
+    .. versionadded:: 2.2
+    """
+
+    def __init__(self, guid=None):
+        """Constructor.
+
+        :param guid: Optional unique identifier for this parameter. If none
+            is specified one will be generated using python hash. This guid
+            will be used when storing parameters in the registry.
+        :type guid: str
         """
-
-        def __init__(self):
-            """Constructor.
-            :rtype : object
-            """
-            super(StringParameter, self).__init__()
-            self.set_expected_type(str)
+        super(StringParameter, self).__init__(guid)
+        self.expected_type = str
