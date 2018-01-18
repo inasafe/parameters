1c06b1dee6227480a58e7b7a7309f8eff1639b62
Ismail Sunni <imajimatika@gmail.com>
2018-01-18 14:55:50 +0700

    Bump version 1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inasafe-parameters',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     license='GPL',
     author='InaSAFE Team',
